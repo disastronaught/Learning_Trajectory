dedde34f3ff9acb51e70519916dfad155bd3d7e5
Odin <139891768+disastronaught@users.noreply.github.com>
2024-01-21 22:21:01 -0500

    Update stackOverFlow.cs

--- C#/stackOverFlow.cs
+++ C#/stackOverFlow.cs
@@ -1,5 +1,5 @@
 //here we're going to take a simple data/progress/iteration tracking program that's extremely light weight and cause a
-//stackoverflow condition by calling 
+//stackoverflow condition by doubly placing a user argument inside of a method we created for output.
 
 
 
@@ -10,7 +10,7 @@ namespace ANoteOnParameters
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main(string[] args)//taking one of the arguments from here in the main method we'll copy it below
     {
       VisitPlanets(3);
       VisitPlanets(4);
@@ -20,7 +20,7 @@ namespace ANoteOnParameters
     static void VisitPlanets(int numberOfPlanets)
     {
       Console.WriteLine($"You visited {numberOfPlanets} new planets...");
-      VisitPlanets(5);
-    }
+      VisitPlanets(5);//when it shows up here it overflows as there's a collision for the function call with what we can only presume is
+    }                 //a doubled reference during the function call. 
   }
 }
