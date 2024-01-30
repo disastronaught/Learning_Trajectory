148a4f8283138e44dd3c83e0b5e3b200df81135b
Odin <139891768+disastronaught@users.noreply.github.com>
2024-01-30 14:28:34 -0500

    Update pyRSSstyle.py

--- Python/pyRSSstyle.py
+++ Python/pyRSSstyle.py
@@ -32,3 +32,19 @@ if not credits >= 120:
 
 if not credits >= 120 and not gpa >=2.0:
   print("You do not meet either requirement to graduate!")
+
+#calculating letter grades in if/elif/else statements. This is crazy fast comming off of a ten day C# binge
+grade = 86
+
+if (grade >= 90):
+  print("A")
+elif(grade >= 80) and (grade <= 90):
+  print("B")
+elif(grade >= 70) and (grade <= 80):
+  print("C")
+elif(grade >= 60) and (grade <= 70):
+  print("D")
+else:
+  print("F")
+
+
