415f77cef62b3e3a04ae7be11266edc3f2881a63
Odin <139891768+disastronaught@users.noreply.github.com>
2024-01-30 00:11:34 -0500

    Create buttonMapTools.cs

---
+++ C#/buttonMapTools.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ButtonArray
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        
+      string[] mapToTools;
+      summerStrut = new string[] {"Dig", "nmap", "dirb", "airmon", "nikto", "hydra", "tracert"};
+
+      int[] callOrder = {1, 2, 3, 4, 5, 6, 7};
+    }
+    static bool CallTool(int, num => int == 1.. //it's getting late and I need to map the libraries with a parallel mapping to GPIO for tools.
+  }
+}
