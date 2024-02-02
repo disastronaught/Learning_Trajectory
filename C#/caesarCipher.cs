0ef81d7e3ae9498f5a9a12a80efe5140c574c87a
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-01 21:43:06 -0500

    Create caesarCipher.cs

---
+++ C#/caesarCipher.cs
@@ -0,0 +1,29 @@
+//this one is way out of line and largely unfinished. I looked away from C# for a day to do 
+//sensor programming on an IR reciever and just didn't give this the attention it needed
+
+using System;
+
+namespace CaesarCipher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      Console.WriteLine("Send your message: ");
+      string message = message.ToLower(Console.ReadLine());
+      
+      char[] messageArray = message.ToCharArray();
+
+      char[] emptyArray = new char[];
+    
+    for (int j = 2; j < 10; j++){}
+
+    char message = letters[];
+
+    int index = Array.IndexOf(letters, alphabet);
+
+    char newLetter = (..);
+    }
+  }
+}
