1ff208f9cc12524d40553110f67ecba78da599e0
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-18 21:00:35 -0500

    Create profile.cs

---
+++ C#/OOYA/profile.cs
@@ -0,0 +1,27 @@
+//one of my last projects in the codecademy C# module is the dating profile. I've saved progress here every day in building out the 
+//program and supplemental library with constructors to run as a fully developed program.
+using System;
+
+namespace DatingProfile
+{ 
+  class Profile
+  {
+    Console.WriteLine("What's your name?");
+    private string name = Console.ReadLine();
+
+    Console.WriteLine("What's your age?");
+    private string age = Console.ReadLine();
+
+    Console.WriteLine("What's your City?");
+    private string city = Console.ReadLine();
+
+    Console.WriteLine("What's your country?");
+    private string country = Console.ReadLine();
+
+    Console.WriteLine("What's your gender?");
+    private string name = Console.ReadLine();
+
+    Console.WriteLine("What are your hobbies?");
+    private string hobbies[] = Console.ReadLine();
+  }
+}
