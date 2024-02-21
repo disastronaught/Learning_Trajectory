3fbed35c31d514ad5015edc421553a39b0df34d0
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-20 22:45:09 -0500

    Create profile2.cs

---
+++ C#/OOYA/profile2.cs
@@ -0,0 +1,31 @@
+//this is the pre-debugged version of an update to the profile script in the dating profile project
+//I'm pretty tired at the moment so in an hour or so tomorrow it'll be debugged and finalized
+using System;
+
+namespace DatingProfile
+{ 
+  class Profile
+  {
+    Console.WriteLine("What's your name?");
+    private string name = Console.ReadLine();
+
+    Console.WriteLine("What's your age?");
+    private int age = Console.ReadLine();
+
+    Console.WriteLine("What's your City?");
+    private string city = Console.ReadLine();
+
+    Console.WriteLine("What's your country?");
+    private string country = Console.ReadLine();
+
+    Console.WriteLine("What's your gender?");
+    private string gender = Console.ReadLine();
+
+    Console.WriteLine("What are your hobbies?");
+    private string hobbies[] = Console.ReadLine();
+  }
+  public Profile(string name, int age, string city, string gender, string hobbies)
+  
+    {this.age = agi; this.age = age; this.city = city; this.country = country; this.gender = gender; this.hobbies = hobbies;}
+  
+}
