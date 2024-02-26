5f48fc18a7ed4ebf8bf4af6bf4aa60a148f4b608
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-26 15:21:52 -0500

    Create emptyVars.go

---
+++ Go/emptyVars.go
@@ -0,0 +1,15 @@
+//seeing how Go assigns empty values to unassinged variables, I made this as a cli output file which prints empty variables on execution
+package main
+
+import "fmt"
+
+func main() {
+
+  var emptyInt int8
+  
+  var emptyFloat float32
+  
+  var emptyString string
+  
+  fmt.Println(emptyInt, emptyFloat, emptyString)
+}
