e1d3b8c0489281889ff845aa898edabd2aa0414a
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-25 21:41:46 -0500

    Create stdcout.cpp

---
+++ C++/stdcout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Zoinks Scoob!\n";
+  
+}
+//Pretty clean compared to what I feel when I write a simple output statement in C#. 
+//I particularly like the way libraries are stated in c++, it reminds me of the data
+//division in IBM COBOL. cbl code had such an organized flow that it really helps
+//when imagining the structure of a new conceptual program
