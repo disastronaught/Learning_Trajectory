445cc9dcb3578197a196d32033efaa8c391a5470
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-26 15:10:24 -0500

    Create declarationsPrac.go

---
+++ Go/declarationsPrac.go
@@ -0,0 +1,20 @@
+//while the declarations may seem "out of order" at this point, it's all the same in the sense that the same information is passed
+//into the vaiables. One thing I like about Go so far, is the ability to state the size of the variables far more accurately and with
+//less typed characters
+//instead of using long/short/4word and so on we can just use ui32..ui8 or ui64 to state the binary length of memory consumed
+package main
+
+import "fmt"
+
+func main() {
+  var numOfFlavors int
+  // Assign a value for numOfFlavors below:
+  numOfFlavors = 57
+  
+  fmt.Println(numOfFlavors)
+  
+    // Declare flavorScale below:
+  var flavorScale float32 = 5.8
+  fmt.Println(flavorScale)
+
+}
