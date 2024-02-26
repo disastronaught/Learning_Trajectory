141285088b1101547a22f55a245831e9e7f3c820
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-26 15:16:58 -0500

    Create strngConcat.go

---
+++ Go/strngConcat.go
@@ -0,0 +1,11 @@
+//getting used to the lack of ';' in this language is quite nice. I think overall it just looks cleaner
+package main
+
+import "fmt"
+
+func main() {
+  // Define a string variable
+  var favoriteSnack string = "almonds"
+
+  fmt.Println("My favorite snack is " + favoriteSnack)
+}
