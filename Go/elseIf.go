3aba01bb53a908a79a6eca0245915128f429fc7d
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-27 16:37:47 -0500

    Create elseIf.go

---
+++ Go/elseIf.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	amountStolen := 64650
+
+	if amountStolen > 1000000 {
+		fmt.Println("We've hit the jackpot!")
+  }
+  else if amountStolen >= 5000 {
+      fmt.Println("NZ doesn't have extradition laws")
+  }
+   else {
+		fmt.Println("Why did we even do this?")
+	}
+}
