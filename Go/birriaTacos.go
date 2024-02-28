0ecbbd6c20d07c5a5101218a2a71aca0eadb9f6a
Odin <139891768+disastronaught@users.noreply.github.com>
2024-02-28 16:35:07 -0500

    Create birriaTacos.go

---
+++ Go/birriaTacos.go
@@ -0,0 +1,15 @@
+//creating functions in Go is more straighforward than in C#, at least for me
+
+package main
+
+import "fmt"
+
+// Define eatTacos() here:
+func eatBirriaTacos() {
+  fmt.Println("Yum!")
+}
+
+func main() {
+  // Call your function here:
+  eatBirriaTacos()
+}
