53226b435d5290403a69ea3fe697c721447757a3
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-02 15:43:58 -0500

    Create userInteractionCLI.cs

---
+++ OffensiveC#/userInteractionCLI.cs
@@ -0,0 +1,17 @@
+//sort of a quick repeat of several C# learning modules from the past. 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the way.");
+
+            Console.Write("Enter your ID number:");
+            string usersChoice = Console.ReadLine();
+
+            Console.WriteLine($"{0} is accepted");
+            Console.ReadKey();
+          
+        }
+    }
+}
