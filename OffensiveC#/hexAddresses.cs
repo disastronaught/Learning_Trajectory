2c04557badbd35a1b3c6bc1481a6316158236839
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-02 15:22:09 -0500

    Create hexAddresses.cs

---
+++ OffensiveC#/hexAddresses.cs
@@ -0,0 +1,26 @@
+//Using two simple variables and some debugging tools in VScode for .NET with C# we're looking at hexidecimal addresses
+//and familiarizing with the use of memory addresses 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffensiveCIntroduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the way.");
+
+            int a = 3;
+            int b = 5;
+
+            Console.WriteLine(a+ b);
+            Console.ReadKey();
+
+        }
+    }
+}
