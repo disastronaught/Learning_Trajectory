3ac89efd9fb4c3b5b01de240ec1b3931dbdab6b2
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-03 15:21:44 -0500

    Update threaded_client.py

--- Python/C2Server/threaded_client.py
+++ Python/C2Server/threaded_client.py
@@ -7,7 +7,13 @@ port_number = 4570
 cs = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 cs.connect((ip_address,port_number))
+msg = input("Enter msg to send: ")
 
+while msg!='quit'
+    cs.send(msg.encode())
+    msg = cs.recv(1024).decode()
+    print(msg)
+    msg = input("Enter msg to send: ")
 #much like a reverse shell, we'll be stating that the target here
 #is the transmitting party, whereas our C2 server is in 'listen' mode
 #or is doing the receiving
