d993a0a11c59c743da8ad5502b62722b321d181b
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-10 23:02:44 +0000

    Create sedanAttributes.cs

---
+++ C#/sedanAttributes.cs
@@ -0,0 +1,40 @@
+//additional setters and getters have been added to the constructors added here
+using System;
+
+namespace LearnInterfaces
+{
+  class Sedan : IAutomobile
+  {
+  	public string LicensePlate
+    { get; }
+
+    public double Speed
+    { get; private set; }
+
+    public int Wheels
+    { get; }
+
+    public Sedan(double speed)
+    {
+      Speed = speed;
+      LicensePlate = Tools.GenerateLicensePlate();
+      Wheels = 4;
+    }
+
+
+    public void Honk()
+    {
+      Console.WriteLine("HONK!");
+    }
+    
+        public void SpeedUp()
+    {
+      Speed += 5;
+    }
+    
+    public void SlowDown()
+    {
+      Speed -= 5;
+    }
+  }
+}
