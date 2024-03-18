22423e679ece62a8d6152c54adc9bdcb1c78043f
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-17 22:04:31 -0400

    Create commandsList.ps1

---
+++ PowerShell/commandsList.ps1
@@ -0,0 +1,12 @@
+Get-Command
+#For use in printing the available cmdlets to the terminal
+Get-Command -Verb Get
+#This prints all of the Get commands
+Get-Command -Noun Host
+#This prints out all of the commands which act on the Host
+Clear-Host
+#Same results as 'clear' in a Unix terminal
+Get-Command -Noun Date
+#This prints all commands relational to the Date object
+
+
