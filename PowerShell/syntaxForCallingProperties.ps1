2d6a150e0459f5aad19b9661e80652399cfe5001
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-21 15:47:02 -0400

    Update syntaxForCallingProperties.ps1

--- PowerShell/syntaxForCallingProperties.ps1
+++ PowerShell/syntaxForCallingProperties.ps1
@@ -5,3 +5,7 @@ $var | Get-Member -MemberType Property
 
 #This is used for printing the methods for this variable
 $my_num | Get-Member -MemberType Method
+
+#this means to me, that we can compare strings or integers, the first thing that comes to mind is a checksum
+#likewise, we can probably use this to check hashes or longer formatted variables to each other instead of
+#doing it digit by digit
