18282aa28be792c6cc1de16e907db8b0f555b7c8
Odin <139891768+disastronaught@users.noreply.github.com>
2024-03-21 15:26:15 -0400

    Create syntaxForCallingProperties.ps1

---
+++ PowerShell/syntaxForCallingProperties.ps1
@@ -0,0 +1,4 @@
+#As we go through the OOP portion and descriptions of this module, I'll be updating this commands list as a reference
+#document I can use while coding in the powershell terminal
+
+$var | Get-Member -MemberType Property
